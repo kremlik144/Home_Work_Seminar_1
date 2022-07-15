3e6414d718c3b7e9985c76bba94168a9d23276aa
«Sergey» <fomin-sm1998@mail.ru>
2022-07-15 04:33:34 +0300

    Задача №8  Домашнее задание к Семинару№1

---
+++ Project_8/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int result = 2;
+
+if (x > 1)
+{
+    while(result <= x)
+    {
+       Console.Write(result);
+       Console.Write(" ");
+       result = result + 2; 
+    } 
+}
+else
+{
+    Console.Write("Четных чисел нет");
+}
